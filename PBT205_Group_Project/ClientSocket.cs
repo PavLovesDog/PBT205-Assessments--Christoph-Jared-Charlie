520e7fa8534587f32e2a1de0b4b7914913b66155
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2022-04-27 15:33:12 +1000

    Added client socket

---
+++ PBT205_Group_Project/ClientSocket.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Net.Sockets;
+using System.Text;
+
+public class ClientSocket
+{
+    public string username;
+    public string password;
+    public Socket socket;
+    public string location;
+}
--- PBT205_Group_Project/contactTracingWindow.Designer.cs
+++ PBT205_Group_Project/contactTracingWindow.Designer.cs
@@ -89,7 +89,7 @@ namespace PBT205_Group_Project
             this.label2.Location = new System.Drawing.Point(56, 0);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(138, 48);
+            this.label2.Size = new System.Drawing.Size(172, 57);
             this.label2.TabIndex = 1;
             this.label2.Text = "Unique ID: 001 - Total\r\n\r\n= PERSON";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -98,10 +98,10 @@ namespace PBT205_Group_Project
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(234, 0);
+            this.label3.Location = new System.Drawing.Point(268, 0);
             this.label3.Margin = new System.Windows.Forms.Padding(38, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(192, 48);
+            this.label3.Size = new System.Drawing.Size(241, 57);
             this.label3.TabIndex = 2;
             this.label3.Text = "         WASD to MOVE\r\n    Num. persons on board:\r\nUnique ID\'s: 000 - Total Perso" +
     "ns";
@@ -110,7 +110,7 @@ namespace PBT205_Group_Project
             // 
             this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
             this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox3.Location = new System.Drawing.Point(488, 2);
+            this.pictureBox3.Location = new System.Drawing.Point(571, 2);
             this.pictureBox3.Margin = new System.Windows.Forms.Padding(60, 2, 2, 2);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(47, 60);
@@ -121,10 +121,10 @@ namespace PBT205_Group_Project
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(539, 0);
+            this.label4.Location = new System.Drawing.Point(2, 64);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(93, 48);
+            this.label4.Size = new System.Drawing.Size(115, 57);
             this.label4.TabIndex = 3;
             this.label4.Text = "Unique ID: 000\r\n\r\n= YOU";
             // 
@@ -135,7 +135,7 @@ namespace PBT205_Group_Project
             this.label1.Location = new System.Drawing.Point(227, 364);
             this.label1.Margin = new System.Windows.Forms.Padding(221, 15, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(185, 14);
+            this.label1.Size = new System.Drawing.Size(231, 14);
             this.label1.TabIndex = 4;
             this.label1.Text = "Search Unique Identifier (ID No.)";
             // 
@@ -157,14 +157,14 @@ namespace PBT205_Group_Project
             this.lblTopic.Location = new System.Drawing.Point(2, 12);
             this.lblTopic.Margin = new System.Windows.Forms.Padding(2, 12, 2, 0);
             this.lblTopic.Name = "lblTopic";
-            this.lblTopic.Size = new System.Drawing.Size(54, 19);
+            this.lblTopic.Size = new System.Drawing.Size(67, 25);
             this.lblTopic.TabIndex = 2;
             this.lblTopic.Text = "TOPIC";
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(223, 0);
+            this.comboBox1.Location = new System.Drawing.Point(236, 0);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(165, 0, 0, 2);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(174, 21);
@@ -173,7 +173,7 @@ namespace PBT205_Group_Project
             // searchButton
             // 
             this.searchButton.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.searchButton.Location = new System.Drawing.Point(397, 0);
+            this.searchButton.Location = new System.Drawing.Point(410, 0);
             this.searchButton.Margin = new System.Windows.Forms.Padding(0, 0, 2, 2);
             this.searchButton.Name = "searchButton";
             this.searchButton.Size = new System.Drawing.Size(56, 19);
